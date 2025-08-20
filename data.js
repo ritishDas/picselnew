9ff9cdba4be453abd234333ef59eb95f625bca84
ritishDas <ritishdas116@outlook.com>
2025-08-21 02:05:22 +0530

    bug fixes

--- data.js
+++ data.js
@@ -15,201 +15,63 @@ const Data = {
       photo:"Asst_Secretory.webp",
       post:"hod" 
     },
-    {
-      name:"kuthesir",
-      photo:"Asst_Secretory.webp",
-      post:"hod" 
-    },
-    {
-      name:"kuthesir",
-      photo:"Asst_Secretory.webp",
-      post:"hod" 
-    },
   ],
   teams:[
     {
       name:"Core",
       head:[
         {
-          name:"aniket",
+          name:"Amartya Choubey",
           photo:"Asst_Secretory.webp",
-          post:"hod" 
+          post:"President"
         },
         {
-          name:"rasika",
+          name:"Ankit Chiklonde",
           photo:"Asst_Secretory.webp",
-          post:"hod" 
-
+          post:"Vice-President"
+        },
+        {
+          name:"Atharva Dayalwar",
+          photo:"Asst_Secretory.webp",
+          post:"Secretary"
+        },
+        {
+          name:"Kumkum Laveti",
+          photo:"Asst_Secretory.webp",
+          post:"Joint-Secretary"
+        },
+        {
+          name:"Ashwini Galhat",
+          photo:"Asst_Secretory.webp",
+          post:"Executive Vice President"
+        },
+        {
+          name:"Mohit Parteti",
+          photo:"Asst_Secretory.webp",
+          post:"Technical Vice President"
         }
       ],
-      asst:[
-        {
-          name:"ritish",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        }
-      ]
+      asst:[]
     },
     {
-      name:"Core",
-      head:[
-        {
-          name:"aniket",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        },
-        {
-          name:"rasika",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-
-        }
+      name:'Treasury',
+      head:[{
+        name:"Vinod Balla",
+        photo:"Asst_Secretory.webp",
+        post:"Treasurer"
+      },
+      {
+        name:"Karan Soyam",
+        photo:"Asst_Secretory.webp",
+        post:"Joint Treasurer"
+      },
+      {
+        name:"Rudresh Dodke",
+        photo:"Asst_Secretory.webp",
+        post:"Joint Treasurer"
+      },
       ],
-      asst:[
-        {
-          name:"ritish",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        }
-      ]
-    },
-    {
-      name:"Core",
-      head:[
-        {
-          name:"aniket",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        },
-        {
-          name:"rasika",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-
-        }
-      ],
-      asst:[
-        {
-          name:"ritish",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        }
-      ]
-    },
-    {
-      name:"Core",
-      head:[
-        {
-          name:"aniket",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        },
-        {
-          name:"rasika",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-
-        }
-      ],
-      asst:[
-        {
-          name:"ritish",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        }
-      ]
-    },
-    {
-      name:"Core",
-      head:[
-        {
-          name:"aniket",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        },
-        {
-          name:"rasika",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-
-        }
-      ],
-      asst:[
-        {
-          name:"ritish",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        }
-      ]
-    },
-    {
-      name:"Core",
-      head:[
-        {
-          name:"aniket",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        },
-        {
-          name:"rasika",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-
-        }
-      ],
-      asst:[
-        {
-          name:"ritish",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        }
-      ]
-    },
-    {
-      name:"Core",
-      head:[
-        {
-          name:"aniket",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        },
-        {
-          name:"rasika",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-
-        }
-      ],
-      asst:[
-        {
-          name:"ritish",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        }
-      ]
-    },
-    {
-      name:"Core",
-      head:[
-        {
-          name:"aniket",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        },
-        {
-          name:"rasika",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-
-        }
-      ],
-      asst:[
-        {
-          name:"ritish",
-          photo:"Asst_Secretory.webp",
-          post:"hod" 
-        }
-      ]
-    },
+      asst:[]
+    }
   ],
 };
